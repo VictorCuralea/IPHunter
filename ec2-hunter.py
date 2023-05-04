1bc4c08ab27dee0a76ad9276392f9e816ac3e6cc
Victor Curalea <victor.curalea@gmail.com>
2023-05-04 09:15:15 +0200

    Update ec2-hunter.py
    fix missing code

--- ec2-hunter.py
+++ ec2-hunter.py
@@ -3,31 +3,29 @@ import boto3
 import argparse
 import sys
 import os
-from concurrent.futures import ThreadPoolExecutor
 
-# other functions remain the same
+def read_ips_from_file(file_path):
+    with open(file_path) as f:
+        return [line.strip() for line in f.readlines()]
 
-def acquire_ip(ecc2, mon_ips, output_file):
-    found = False
-    while not found:
-        allocation = ecc2.allocate_address(Domain="vpc")
-        address = allocation["PublicIp"]
-        allocation_id = allocation["AllocationId"]
-        if address in mon_ips:
-            found = True
-            print_and_log("Acquired IP {0}".format(address), output_file)
-            print_and_log("Found IP: {0}".format(address), output_file)
-        else:
-            print_and_log("Generated IP {0} does not match desired IPs. Releasing...".format(address), output_file)
-            ecc2.release_address(AllocationId=allocation_id)
-            time.sleep(60)
+def print_and_log(message, output_file):
+    print(message)
+    with open(output_file, 'a') as output:
+        os.write(output.fileno(), (message + '\n').encode())
 
 if __name__ == "__main__":
-    # parse arguments
-    # same as before
+    parser = argparse.ArgumentParser(description="Acquire specific Elastic IP addresses in AWS")
+    parser.add_argument("-region", dest="region", help="AWS Region to use", required=True)
+    parser.add_argument("-ips", dest="ip_file", help="File containing IP addresses, one per line", required=True)
+    parser.add_argument("-awsid", dest="AWSAccessKeyId", help="Your AWS access key ID", required=True)
+    parser.add_argument("-awssecret", dest="AWSSecretKey", help="Your AWS secret access key", required=True)
+    parser.add_argument("-o", dest="output_file", help="Output file to store the results", required=True)
+    args = parser.parse_args(sys.argv[1:])
 
+    found = False
     mon_ips = read_ips_from_file(args.ip_file)
 
+    # connect to ec2 service with provided keys
     ecc2 = boto3.client(
         "ec2",
         aws_access_key_id=args.AWSAccessKeyId,
@@ -35,7 +33,17 @@ if __name__ == "__main__":
         region_name=args.region
     )
 
-    num_threads = 10  # Adjust this number based on the number of concurrent threads you want
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        for _ in range(num_threads):
-            executor.submit(acquire_ip, ecc2, mon_ips, args.output_file)
+    # acquiring Elastic IP and release it until we acquire specific IP
+    while not found:
+        allocation = ecc2.allocate_address(Domain="vpc")
+        address = allocation["PublicIp"]
+        allocation_id = allocation["AllocationId"]
+        if address in mon_ips:
+            found = True
+            print_and_log("Acquired IP {0}".format(address), args.output_file)
+            print_and_log("Found IP: {0}".format(address), args.output_file)
+        else:
+            print_and_log("Generated IP {0} does not match desired IPs. Releasing...".format(address), args.output_file)
+            ecc2.release_address(AllocationId=allocation_id)
+            # make sure to get new addresses
+            time.sleep(60)
